97ff3fa39ea907f7c52c3aa05f912af1703e5dcc
Hamilton <hfantin@gmail.com>
2019-07-19 12:23:48 -0300

    alter database password

--- cap3/licensing-service/build.gradle.kts
+++ cap3/licensing-service/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.security:spring-security-rsa")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	runtimeOnly("org.postgresql:postgresql")
