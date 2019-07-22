7417977cefd59c8788df4104e9437b0da5e1bff5
Hamilton <hfantin@gmail.com>
2019-07-22 11:54:00 -0300

    divide project into branchs per chapter - cap1

--- cap2/licensing-service/src/main/kotlin/com/github/hfantin/licensingservice/model/License.kt
+++
@@ -1,7 +0,0 @@
-package com.github.hfantin.licensingservice.model
-
-data class License(val id: String,
-                   val organizationId: String,
-                   val productName: String,
-                   val licenseType: String
-)
--- hello-world/settings.gradle.kts
+++ hello-world/settings.gradle.kts
@@ -3,4 +3,4 @@ pluginManagement {
 		gradlePluginPortal()
 	}
 }
-rootProject.name = "cap1"
+rootProject.name = "hello-world"
