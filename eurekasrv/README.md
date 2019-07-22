3515c80c5231645987ca9d05dab8e3ca6ab243c9
Hamilton <hfantin@gmail.com>
2019-07-22 13:18:48 -0300

    include the dependencies to run in java 9+

--- eurekasrv/README.md
+++ eurekasrv/README.md
@@ -1,1 +1,3 @@
 # this is the eureka server
+
+- [localhost:8761](http://localhost:8761)
--- eurekasrv/build.gradle.kts
+++ eurekasrv/build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.1.6.RELEASE"
 	id("io.spring.dependency-management") version "1.0.7.RELEASE"
-	kotlin("jvm") version "1.2.71"
-	kotlin("plugin.spring") version "1.2.71"
+	kotlin("jvm") version "1.3.41"
+	kotlin("plugin.spring") version "1.3.41"
 }
 
 group = "com.github.hfantin"
@@ -20,6 +20,11 @@ extra["springCloudVersion"] = "Greenwich.SR2"
 dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	// dependencies to work with java 9+
+	implementation("javax.xml.bind:jaxb-api:2.3.0") 	// 2.3.0
+	implementation("com.sun.xml.bind:jaxb-impl:2.3.0") //2.3.0
+	implementation("org.glassfish.jaxb:jaxb-runtime:2.3.0") //2.3.0
+	implementation("javax.activation:activation:1.1.1") //1.1.1
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
