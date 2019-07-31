c9eb70a6f6245ac52afcb8790f64423d346e4a18
Hamilton Geraldo Fantin <hfantin@gmail.com>
2019-07-31 11:42:39 -0300

    update docker files

--- licensing-service/run.sh
+++ licensing-service/run.sh
@@ -20,10 +20,9 @@ while ! `nc -z configserver $CONFIGSERVER_PORT`; do sleep 3; done
 echo "*******  Configuration Server has started"
 
 echo "********************************************************"
-echo "Starting Organization Service  "
+echo "Starting License Server with Configuration Service via Eureka :  $EUREKASERVER_URI" ON PORT: $SERVER_PORT;
 echo "********************************************************"
 java -Djava.security.egd=file:/dev/./urandom -Dserver.port=$SERVER_PORT   \
      -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI             \
      -Dspring.cloud.config.uri=$CONFIGSERVER_URI                          \
-     -Dspring.profiles.active=$PROFILE                                   \
-     -jar app.jar
+     -Dspring.profiles.active=$PROFILE -jar app.jar
