a41c2c6cb22592847084383bb26126d932a227df
Hamilton Geraldo Fantin <hfantin@gmail.com>
2019-08-14 20:44:49 -0300

    new auth service

---
+++ authentication-service/src/test/kotlin/com/github/hfantin/authenticationservice/AuthenticationServiceApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.github.hfantin.authenticationservice
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
