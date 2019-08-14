ea368603d8142018f64d6897ad4790a586d13784
Hamilton Geraldo Fantin <hfantin@gmail.com>
2019-08-13 23:12:16 -0300

    adjusts docker file

--- orgservice-new/run.sh
+++ orgservice-new/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 echo "********************************************************"
 echo "Waiting for the eureka server to start on port $EUREKASERVER_PORT"
 echo "********************************************************"
@@ -19,10 +20,10 @@ while ! `nc -z configserver $CONFIGSERVER_PORT`; do sleep 3; done
 echo "*******  Configuration Server has started"
 
 echo "********************************************************"
-echo "Starting New Organization Service                           "
+echo "Starting Organization Service  "
 echo "********************************************************"
 java -Djava.security.egd=file:/dev/./urandom -Dserver.port=$SERVER_PORT   \
      -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI             \
      -Dspring.cloud.config.uri=$CONFIGSERVER_URI                          \
      -Dspring.profiles.active=$PROFILE                                   \
-     -jar /usr/local/organizationservice-new/@project.build.finalName@.jar
+     -jar app.jar
--- specialroutes-service/run.sh
+++ specialroutes-service/run.sh
@@ -25,4 +25,4 @@ java -Djava.security.egd=file:/dev/./urandom -Dserver.port=$SERVER_PORT   \
      -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI             \
      -Dspring.cloud.config.uri=$CONFIGSERVER_URI                          \
      -Dspring.profiles.active=$PROFILE                                   \
-     -jar /usr/local/specialroutes-service/@project.build.finalName@.jar
+     -jar app.jar
