da17ec08efa3efae10529b516a81b77829699b4e
Hamilton Geraldo Fantin <hfantin@gmail.com>
2019-09-02 21:25:38 -0300

    adjusts docker

--- licensing-service/src/main/kotlin/com/github/hfantin/licensingservice/consumers/Consumer.kt
+++ licensing-service/src/main/kotlin/com/github/hfantin/licensingservice/consumers/Consumer.kt
@@ -1,6 +1,5 @@
 package com.github.hfantin.licensingservice.consumers
 
-import com.github.hfantin.licensingservice.model.OrganizationChangeDto
 import org.slf4j.LoggerFactory
 import org.springframework.cloud.stream.annotation.EnableBinding
 import org.springframework.cloud.stream.annotation.StreamListener
@@ -10,9 +9,9 @@ import org.springframework.cloud.stream.messaging.Sink
 class Consumer {
     private val logger = LoggerFactory.getLogger(javaClass.simpleName)
     @StreamListener(Sink.INPUT)
-    fun loggerSink(dto: OrganizationChangeDto)
+    fun loggerSink(dto: String)
     {
-        logger.debug("Received an event for organization id {}", dto.id);
+        logger.info("Received an event for organization id {}", dto)
     }
 }
 
